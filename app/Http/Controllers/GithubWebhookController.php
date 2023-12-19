d43a5398b91a2557080de88826426ac553cede73
TWithers <timothy.withers@gmail.com>
2023-12-19 16:21:00 +0000

    Another

--- app/Http/Controllers/GithubWebhookController.php
+++ app/Http/Controllers/GithubWebhookController.php
@@ -15,14 +15,23 @@ class GithubWebhookController extends Controller
         $event = $request->header('x-github-event');
         $branch = $request->get('ref');
 
+        $response = [
+            'message' => 'ok',
+            'event' => $event,
+            'branch' => $branch,
+            'action' => 'nothing',
+        ];
+
         if ($event !== 'push' && (str($branch)->endsWith('main') || str($branch)->endsWith('master'))) {
-            return response()->json(['message' => 'ok', 'event' => $event, 'branch' => $branch]);
+            return response()->json($response);
         }
 
-//        $api = app(TuyaConnector::class);
-//        $api->auth()->getToken();
-//        $api->devices()->sendProperties(config('services.tuya.deviceId'), ['manual_feed' => 1]);
+        $response['action'] = 'dispense';
 
-        return response()->json(['message' => 'ok', 'event' => $event, 'branch' => $branch]);
+        $api = app(TuyaConnector::class);
+        $api->auth()->getToken();
+        $api->devices()->sendProperties(config('services.tuya.deviceId'), ['manual_feed' => 1]);
+
+        return response()->json($response);
     }
 }
